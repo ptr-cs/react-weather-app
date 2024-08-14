a645f8092ba51c5f5308a3331110b3d89cb70cff
blah <blah@blah.blah>
2024-08-14 11:15:32 -0400

    fixed unit test error for PastPrecipitation component

--- src/components/PastPrecipitation/PastPrecipitation.test.tsx
+++ src/components/PastPrecipitation/PastPrecipitation.test.tsx
@@ -5,8 +5,13 @@ import '@testing-library/jest-dom';
 import PastPrecipitation from './PastPrecipitation';
 
 test('renders PastPrecipitation component with canvas and summary text', () => {
-    const data = [1,2,3];
-    const { getByText, getByRole } = render(<PastPrecipitation data={data}/>);
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+  }))  
+  const data = [1,2,3];
+  const { getByText, getByRole } = render(<PastPrecipitation data={data}/>);
 
   // Check if the summary text is rendered correctly
   expect(getByText('Past Precip. Summary (in)')).toBeInTheDocument();
